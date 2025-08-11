327a817ca73763f6323503d689432408d3e641d8
Nathan Harris <singdubharris@gmail.com>
2025-08-11 14:49:38 -0400

    working

--- components/compliancetool.jsx
+++ components/compliancetool.jsx
@@ -8,7 +8,23 @@ function ComplianceTool(){
 
     return(
         <>
-       
+        <div>
+            <div>BRAKE SYSTEM</div>
+            <div>Service Brakes</div>
+            <div>Parking Brake System</div>
+            <div>Brake Drums or Rotors</div>
+            <div>Brake Hose</div>
+            <div>Brake Tubing</div>
+            <div>Low Pressure Warning Device</div>
+            <div>Tractor Protection Valve</div>
+            <div>Air Compressor</div>
+            <div>Electric Brakes</div>
+            <div>Hydraulic Brakes</div>
+            <div>Vacuum Systems</div>
+            <div>Antilock Brake System</div>
+            <div>Automatic Brake Adjusters</div>
+        </div>
+
 
         </>
     )
